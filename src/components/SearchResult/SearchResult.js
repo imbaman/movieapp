e55182a61fa3019b4dc749758efd36689c6c922c
imbaman <imbaman2@gmail.com>
2021-05-30 21:09:53 +0200

    search result component

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -1,5 +1,22 @@
-const SearchResult = () => {
-  return <div>Search Result</div>;
+import { useHistory, Link } from "react-router-dom";
+
+const SearchResult = ({ search }) => {
+  const history = useHistory();
+
+  const img_api = "https://image.tmdb.org/t/p/w185";
+  return (
+    <div className='searched-box'>
+      <div className='img-container'>
+        <img src='' alt='' />
+      </div>
+      <div className='content'>
+        <h1>{`Top result for: ${search}`}</h1>
+        <h2>Title</h2>
+        <small>genre year</small>
+        <p>description</p>
+      </div>
+    </div>
+  );
 };
 
 export default SearchResult;
