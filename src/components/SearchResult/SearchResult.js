4603cd77d98650ed051519d0d1e75e2a2d8cb244
imbaman <imbaman2@gmail.com>
2021-05-31 13:03:52 +0200

    data map

--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -1,21 +1,40 @@
 import { useHistory, Link } from "react-router-dom";
+import "./SearchResult.css";
 
 const SearchResult = ({ search }) => {
   const history = useHistory();
+  const movieData = history.location.movies;
+  const img = "https://image.tmdb.org/t/p/w185";
 
-  const img_api = "https://image.tmdb.org/t/p/w185";
+  console.log(movieData);
+
   return (
-    <div className='searched-box'>
-      <div className='img-container'>
-        <img src='' alt='' />
-      </div>
-      <div className='content'>
-        <h1>{`Top result for: ${search}`}</h1>
-        <h2>Title</h2>
-        <small>genre year</small>
-        <p>description</p>
-      </div>
-    </div>
+    <>
+      <h2 className='results'>
+        Top Results for <span className='bold'>"{search}"</span>
+      </h2>
+      {movieData ? (
+        movieData.map((movie) => (
+          <div className='searchResult'>
+            <div className='searchResult__img'>
+              <img src={img + movie.poster_path} alt='poster' />
+            </div>
+            <div className='searchResult__text'>
+              <h3>{movie.title}</h3>
+              <small>{movie.release_date}</small>
+              <p>{movie.overview}</p>
+            </div>
+            <div className='searchResult__score'>
+              <p className='score'>{movie.vote_average}</p>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div>
+          <p>no data</p>
+        </div>
+      )}
+    </>
   );
 };
 
