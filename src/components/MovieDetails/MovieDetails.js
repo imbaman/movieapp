dc6dea986b2a4a3e17e775077d3826b3b9cc49f1
imbaman <imbaman2@gmail.com>
2021-06-03 21:40:06 +0200

    movie detail link

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,5 +1,20 @@
-const MovieDetails = () => {
-  return <div>MovieDetail</div>;
+import { useEffect, useState } from "react";
+import { instance } from "../../api";
+
+const MovieDetails = ({ match }) => {
+  const [movie, setMovie] = useState("");
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await instance.get(`/movie/${match.params.id}`);
+      setMovie(response.data);
+      console.log("saddas");
+      return response;
+    };
+    fetchMovies();
+  }, []);
+
+  return <div>movie det {movie.title}</div>;
 };
 
 export default MovieDetails;
