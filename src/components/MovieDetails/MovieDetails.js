2d5185434f73200668318e16f078e53a00920a6f
imbaman <imbaman2@gmail.com>
2021-06-09 22:05:19 +0200

    movie det test content

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import { instance } from "../../api";
+import "./MovieDetails.css";
 
 const MovieDetails = ({ match }) => {
   const [movie, setMovie] = useState("");
-
+  const img = "https://image.tmdb.org/t/p/w185";
   useEffect(() => {
     const fetchMovies = async () => {
       const response = await instance.get(`/movie/${match.params.id}`);
@@ -14,7 +15,29 @@ const MovieDetails = ({ match }) => {
     fetchMovies();
   }, []);
 
-  return <div>movie det {movie.title}</div>;
+  return (
+    <div className='container details'>
+      <div className='details__left'>
+        <h3>{movie.title}</h3>
+        <div className='score-container'>
+          {" "}
+          <p
+            className={`score  ${
+              movie.vote_average > 6 ? "score-up" : "score-low"
+            }`}>
+            {movie.vote_average}
+          </p>
+          <p className='border'>based on {movie.vote_count} counts</p>
+        </div>
+
+        <p>RELEASE DATE : {movie.release_date}</p>
+      </div>
+      <div className='details__right'>
+        movie trailer?
+        <img src={img + movie.poster_path} alt='' />
+      </div>
+    </div>
+  );
 };
 
 export default MovieDetails;
